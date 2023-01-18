2562fc6bc2e869af21ba3a51325ef641b0033b6f
Artem <91430261+Silencer66@users.noreply.github.com>
2023-01-18 13:25:50 +0200

    Add files via upload

---
+++ FIFTEEN c#/FormGame15.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FIFTEEN
+{
+    public partial class FormGame15 : Form
+    {
+        Game game;
+        int count = 0, time = 0;
+        public FormGame15()
+        {
+            InitializeComponent();
+            game = new Game(4);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            count++;
+            int position = Convert.ToInt32(((Button)sender).Tag); //получаем с помощью sender какая кнопка нажата
+            //button(position).Text = position.ToString(); //присваеваем текст нажатой кнопке
+            //MessageBox.Show(position.ToString());
+            game.shift(position);
+            Refresh();
+            if (game.check_numbers())
+            {
+                MessageBox.Show($"Победа! Ходов: {count}, За время: {time}секунд. ");
+                timer1.Enabled = false;
+                start_game();
+            }
+        }
+        private Button button(int position)
+        {
+            switch (position)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default: return null;
+            }
+        }
+        //запуск формы
+        private void FormGame15_Load(object sender, EventArgs e)
+        {
+            start_game();
+        }
+        // кнопкв начать игру 
+        private void menu_start_Click(object sender, EventArgs e)
+        {
+            start_game();
+        }
+
+        private void start_game()
+        {
+            game.start();
+            for (int j = 0; j < 110; j++)
+                game.shift_random();
+            Refresh();
+            count = 0;
+            timer1.Enabled = true;
+            time = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+        }
+
+        private void Refresh()
+        {
+            for (int position = 0; position < 16; position++)
+            {
+                int number = game.get_number(position);
+                button(position).Text = number.ToString();
+                button(position).Visible = (number > 0);
+            }
+        }
+    }
+}
---
+++ FIFTEEN c#/Game.cs
@@ -0,0 +1,105 @@
+﻿using System;
+namespace FIFTEEN
+{
+    class Game
+    {
+        int size;
+        int[,] map; //матрица
+        int space_x, space_y; //координаты пустой клетки 
+        static Random rand = new Random();
+        public Game(int size) //size - размер нашего поля (4х4)
+        { //конструктор
+            if (size < 2)
+                size = 2;
+            if (size > 5)
+                size = 5;
+            this.size = size;
+            map = new int[size, size]; //создаем карту 
+        }
+        public void start()
+        {
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    map[x, y] = coords_to_position(x, y) + 1;
+            space_x = size - 1;
+            space_y = size - 1;
+            map[space_x, space_y] = 0; // свободное место 
+        }
+        public void shift(int position)
+        {
+            int x, y;
+            position_to_coords(position, out x, out y);
+            if (Math.Abs(space_x - x) + Math.Abs(space_y - y) != 1)
+                return;
+            map[space_x, space_y] = map[x, y];
+            map[x, y] = 0;
+            space_x = x;
+            space_y = y;
+        }
+        public void shift_random()
+        {
+            //shift(rand.Next(0, size * size));
+            int a = rand.Next(0, 4); //рандомное число от 0 до 4
+            int x = space_x; // запоминаем координаты пустой ячейки в x и y
+            int y = space_y;
+            switch (a)
+            {
+                case 0:
+                    x--;
+                    break;
+                case 1:
+                    x++;
+                    break;
+                case 2:
+                    y--;
+                    break;
+                case 3:
+                    y++;
+                    break;
+            }
+            shift(coords_to_position(x, y));
+        }
+        //проверка закончена ли игра
+        public bool check_numbers()
+        {
+            if (!(space_x == size - 1 && space_y == size - 1)) //если пустая не в полседней позиции
+                return false;
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    if (!(x == size - 1 && y == size - 1)) //если не последняя ячейка
+                        if (map[x, y] != coords_to_position(x, y) + 1)
+                            return false;
+            return true;
+        }
+
+        public int get_number(int position) //передается номер кнопки 
+        {
+            int x, y;
+            position_to_coords(position, out x, out y);//передаем позицию а получаем координаты
+            if (x < 0 || x >= size) return 0;
+            if (y < 0 || y >= size) return 0;
+            return map[x, y]; // получим позицию
+        }
+        private int coords_to_position(int x, int y)
+        {
+            if (x < 0)
+                x = 0;
+            if (x > size - 1)
+                x = size - 1;
+            if (y < 0)
+                y = 0;
+            if (y > size - 1)
+                y = size - 1;
+            return y * size + x;
+        }
+        private void position_to_coords(int position, out int x, out int y) //два выходных параметра
+        {
+            if (position < 0)
+                position = 0;
+            if (position > size * size - 1)
+                position = size * size - 1;
+            x = position % size;
+            y = position / size;
+        }
+    }
+}
